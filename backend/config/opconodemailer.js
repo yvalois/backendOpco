df1167e2cc83587f6b0bce92878bb04981a20fae
yvalois <yeison.valopez@gmail.com>
2023-06-09 19:09:01 -0500

    Enviroment fix

--- backend/config/opconodemailer.js
+++ backend/config/opconodemailer.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 
-const host = process.env.OPCO_HOST;
-const user = process.env.OPCO_USER;
-const pass = process.env.OPCO_PASSWORD;
+const host = process.env.REACT_APP_OPCO_HOST;
+const user = process.env.REACT_APP_OPCO_USER;
+const pass = process.env.REACT_APP_OPCO_PASSWORD;
 
 
 const opcoMail = nodemailer.createTransport({
